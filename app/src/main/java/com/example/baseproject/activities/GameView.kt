7377f0a8c4973ba1e8c3d69686b2d3cb8baa1562
AnhTT SSquad <anhtt@dktechgroup.com>
2025-12-15 18:29:16 +0700

    add gameview

--- app/src/main/java/com/example/baseproject/activities/GameView.kt
+++ app/src/main/java/com/example/baseproject/activities/GameView.kt
@@ -14,16 +14,12 @@ import kotlin.coroutines.Continuation
 class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     private val dotPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val dotPaintDefault = Color.BLACK
 
     private val linePaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val linePaintDefault = Color.GREEN
 
     private val playerOneBoxPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val playerOneBoxPaintDefault = Color.CYAN
 
     private val playerTwoBoxPaint = Paint(Paint.ANTI_ALIAS_FLAG)
-    private val playerTwoBoxPaintDefault = Color.YELLOW
 
     init {
         val typedArray = context.theme.obtainStyledAttributes(attrs,R.styleable.GameView, 0,0)
@@ -31,6 +27,16 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         try {
 
             val dotPaintColor = typedArray.getColor(R.styleable.GameView_dotsColor, Color.BLACK)
+            dotPaint.color = dotPaintColor
+
+            val linePainColor = typedArray.getColor(R.styleable.GameView_lineColor, Color.GREEN)
+            linePaint.color = linePainColor
+
+            val playerOneColor = typedArray.getColor(R.styleable.GameView_player_1_color, Color.RED)
+            playerOneBoxPaint.color = playerOneColor
+
+            val playerTwoColor = typedArray.getColor(R.styleable.GameView_player_2_color, Color.CYAN)
+            playerTwoBoxPaint.color = playerTwoColor
 
         } catch (e: Exception){
             Toast.makeText(context, "Loi gameview: ${e.message} - ${e.printStackTrace()}", Toast.LENGTH_LONG).show()
@@ -43,6 +49,10 @@ class GameView(context: Context, attrs: AttributeSet) : View(context, attrs) {
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        val viewWidth = width.toFloat()
+        val viewHeight = height.toFloat()
+
 
     }
 }
